ec8f6d2a0e9894af85d37d531c6cddf8dfd131ff
rookiesky <1332305055@qq.com>
2018-05-11 15:54:56 +0800

    add v0.2

--- app/Tools/CloudUpload.php
+++ app/Tools/CloudUpload.php
@@ -11,7 +11,8 @@ class CloudUpload
     {
         $config = getConfig('upload');
         try{
-            $upload = new UploadFile($config['OSS_ACCESSKEY'],$config['OSS_ACCESS_KEY_SECRET'],$config['OSS_BUCKET'],$config['OSS_ENDPOINT'],'oss');
+            //$upload = new UploadFile($config['OSS_ACCESSKEY'],$config['OSS_ACCESS_KEY_SECRET'],$config['OSS_BUCKET'],$config['OSS_ENDPOINT'],'oss');
+            $upload = new UploadFile($config['QINIU_ACCESSKEY'],$config['QINIU_SECRETKEY'],$config['QINIU_BUCKET']);
             return $upload->uploadManager();
         }catch (\Exception $e){
             return ['error'=>$e->getMessage()];
